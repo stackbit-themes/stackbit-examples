8b21bbaaae672f59ba71be2541e691c7d410015b
Sean C Davis <scdavis41@gmail.com>
2022-08-10 13:58:06 -0400

    Setup contentful tutorial

--- tutorial-nextjs-contentful/next.config.js
+++ tutorial-nextjs-contentful/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
