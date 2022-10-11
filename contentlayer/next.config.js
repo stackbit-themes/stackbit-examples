177bbe7d1a33fede722ed50c2beedd308a5f5ed6
Sean C Davis <scdavis41@gmail.com>
2022-10-11 13:27:56 -0400

    Back to main config

--- contentlayer/next.config.js
+++ contentlayer/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
-const { withContentlayer } = require('next-contentlayer');
-const { withStackbit } = require('experimental-next-stackbit');
+// const { withContentlayer } = require('next-contentlayer');
+// const { withStackbit } = require('experimental-next-stackbit');
 
-module.exports = withStackbit(
-  withContentlayer({
-    // webpack: (config, { dev }) => {
-    //   // Makes webpack not trigger recompiling when files in the content folder are updated.
-    //   config.watchOptions.ignored.push('**/content/**');
-    //   return config;
-    // },
-    reactStrictMode: true,
-  }),
-);
+const config = {
+  webpack: (config, { dev }) => {
+    // Makes webpack not trigger recompiling when files in the content folder are updated.
+    config.watchOptions.ignored.push('**/content/**');
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+module.exports = config;
+// module.exports = withStackbit(withContentlayer(config));
