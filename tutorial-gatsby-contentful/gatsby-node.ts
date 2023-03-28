035e89f38a5088ec2aa7f400a03dd65934297e2a
Sean C Davis <scdavis41@gmail.com>
2023-03-28 13:19:24 -0400

    Formatting

--- tutorial-gatsby-contentful/gatsby-node.ts
+++ tutorial-gatsby-contentful/gatsby-node.ts
@@ -1,39 +1,36 @@
-import path from "path";
+import path from 'path';
 
-import type { GatsbyNode } from "gatsby";
+import type { GatsbyNode } from 'gatsby';
 
 type ContentfulPage = {
-  contentful_id: string;
-  slug: string;
+    contentful_id: string;
+    slug: string;
 };
 
 type PageQueryResult = {
-  errors?: any;
-  data?: { allContentfulPage: { nodes: ContentfulPage[] } };
+    errors?: any;
+    data?: { allContentfulPage: { nodes: ContentfulPage[] } };
 };
 
-export const createPages: GatsbyNode["createPages"] = async ({
-  graphql,
-  actions,
-}) => {
-  const { createPage } = actions;
-  const ComposablePage = path.resolve(`src/templates/composable-page.tsx`);
-  const result: PageQueryResult = await graphql(`
-    query PageGeneratorQuery {
-      allContentfulPage {
-        nodes {
-          contentful_id
-          slug
+export const createPages: GatsbyNode['createPages'] = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+    const ComposablePage = path.resolve(`src/templates/composable-page.tsx`);
+    const result: PageQueryResult = await graphql(`
+        query PageGeneratorQuery {
+            allContentfulPage {
+                nodes {
+                    contentful_id
+                    slug
+                }
+            }
         }
-      }
-    }
-  `);
+    `);
 
-  result.data?.allContentfulPage.nodes.forEach((edge: ContentfulPage) => {
-    createPage({
-      path: `${edge.slug}`,
-      component: ComposablePage,
-      context: { slug: edge.slug },
+    result.data?.allContentfulPage.nodes.forEach((edge: ContentfulPage) => {
+        createPage({
+            path: `${edge.slug}`,
+            component: ComposablePage,
+            context: { slug: edge.slug }
+        });
     });
-  });
 };
