09f8e099d13a80bfd92b2c661b50a760e09fd0ba
Elad Rosenheim <elad@stackbit.com>
2023-04-23 12:47:58 +0300

    Add import object to SB config

--- gatsby-contentful/stackbit.config.ts
+++ gatsby-contentful/stackbit.config.ts
@@ -13,5 +13,16 @@ export default defineStackbitConfig({
             accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!
         })
     ],
-    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }]
+    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
+    // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
+    import: {
+        type: 'contentful',
+        contentFile: 'contentful/export.json',
+        uploadAssets: true,
+        assetsDirectory: 'contentful',
+        spaceIdEnvVar: 'CONTENTFUL_SPACE_ID',
+        deliveryTokenEnvVar: 'CONTENTFUL_DELIVERY_TOKEN',
+        previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
+        accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN'
+    }
 });
