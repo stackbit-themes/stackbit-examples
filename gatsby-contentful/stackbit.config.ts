62e2d82db591c5348e9ad332e5639220c534e3cd
Elad Rosenheim <elad@stackbit.com>
2023-04-23 12:45:40 +0300

    Update stackbit.config.ts

--- gatsby-contentful/stackbit.config.ts
+++ gatsby-contentful/stackbit.config.ts
@@ -3,6 +3,7 @@ import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
     stackbitVersion: '~0.6.0',
+    nodeVersion: '18',
     ssgName: 'gatsby',
     contentSources: [
         new ContentfulContentSource({
