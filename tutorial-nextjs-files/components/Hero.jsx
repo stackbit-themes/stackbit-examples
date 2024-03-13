4fc3fa86869f2b62a5c1b3493917691f6105e40f
TomasBankauskas <tbdizainas@gmail.com>
2024-03-13 18:12:02 +0200

    updated tutorial-nextjs-files example

--- tutorial-nextjs-files/components/Hero.jsx
+++ tutorial-nextjs-files/components/Hero.jsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 import { Button } from './Button.jsx';
 
 const themeClassMap = {
-    imgLeft: 'flex-row-reverse',
-    imgRight: ''
+    imgLeft: 'md:flex-row-reverse',
+    imgRight: 'md:flex-row'
 };
 
 export const Hero = (props) => {
     return (
-        <div className="px-12 py-24 bg-gray-100">
-            <div className={`flex mx-auto max-w-6xl gap-12 ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
-                <div className="max-w-xl py-20 mx-auto lg:shrink-0">
+        <div className="px-6 py-16 bg-gray-100 sm:px-12 sm:py-24">
+            <div className={`max-w-6xl mx-auto flex flex-col gap-12 md:items-center ${themeClassMap[props.theme] ?? themeClassMap['imgRight']}`}>
+                <div className="flex-1 w-full max-w-xl mx-auto">
                     <h1 className="mb-6 text-5xl leading-tight">{props.heading}</h1>
                     {props.body && (
                         <Markdown options={{ forceBlock: true }} className="mb-6 text-lg">
@@ -20,8 +20,16 @@ export const Hero = (props) => {
                     )}
                     {props.button && <Button {...props.button} />}
                 </div>
-                <div className="relative hidden w-full overflow-hidden rounded-md lg:block">
-                    {props.image && <Image src={props.image.src} alt={props.image.alt} layout="fill" objectFit="contain" />}
+                <div className="w-full aspect-[4/3] flex-1 relative overflow-hidden rounded-md">
+                    {props.image && (
+                        <Image
+                            src={props.image.src}
+                            alt={props.image.alt}
+                            fill
+                            className="object-cover"
+                            sizes="(max-width: 767px) 100vw, (max-width: 1200px) 50vw, 600px"
+                        />
+                    )}
                 </div>
             </div>
         </div>
