cce4f6369efa8c99a2063e8dddad4a89f403deb2
Tomas Bankauskas <tbdizainas@gmail.com>
2024-03-19 10:13:42 +0200

    Removing chakra UI (#106)
    * removed chakra-ui
    
    * Update README.md

--- chakra-ui/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    webpack: (config, { dev }) => {
-        // Makes webpack not trigger recompiling when files in the content folder are updated.
-        config.watchOptions.ignored.push('**/content/**');
-        return config;
-    }
-};
